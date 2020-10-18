967031d55ebd09e93387c69650e165791329c5c3
unknown <q2651078496@outlook.com>
2020-10-18 22:22:19 +0800

    更改

--- src/pers/tany/customchunklimit/BasicLibrary.java
+++ src/pers/tany/customchunklimit/BasicLibrary.java
@@ -10,6 +10,7 @@ import org.bukkit.inventory.ItemStack;
 public class BasicLibrary {
 	public static List<EntityType> uselessentitylist = new ArrayList<EntityType>();
 	public static List<ItemStack> stainedglass = new ArrayList<ItemStack>();
+	public static List<ItemStack> stainedwool = new ArrayList<ItemStack>();
 	
 	BasicLibrary(){
 		uselessentitylist.add(EntityType.ZOMBIE);
@@ -77,6 +78,33 @@ public class BasicLibrary {
 				ItemStack glass = new ItemStack(Material.valueOf("STAINED_GLASS_PANE"));
 				glass.setDurability((short) i);
 				stainedglass.add(glass);
+			}
+		}
+		try {
+			stainedglass.add(new ItemStack(Material.valueOf("WHITE_WOOL")));
+			stainedglass.add(new ItemStack(Material.valueOf("ORANGE_WOOL")));
+			stainedglass.add(new ItemStack(Material.valueOf("MAGENTA_WOOL")));
+			stainedglass.add(new ItemStack(Material.valueOf("LIGHT_BLUE_WOOL")));
+			stainedglass.add(new ItemStack(Material.valueOf("YELLOW_WOOL")));
+			stainedglass.add(new ItemStack(Material.valueOf("LIME_WOOL")));
+			
+			stainedglass.add(new ItemStack(Material.valueOf("PINK_WOOL")));
+			stainedglass.add(new ItemStack(Material.valueOf("GRAY_WOOL")));
+			stainedglass.add(new ItemStack(Material.valueOf("LIGHT_GRAY_WOOL")));
+			stainedglass.add(new ItemStack(Material.valueOf("CYAN_WOOL")));
+			stainedglass.add(new ItemStack(Material.valueOf("PURPLE_WOOL")));
+			stainedglass.add(new ItemStack(Material.valueOf("BLUE_WOOL")));
+			
+			stainedglass.add(new ItemStack(Material.valueOf("BROWN_WOOL")));
+			stainedglass.add(new ItemStack(Material.valueOf("GREEN_WOOL")));
+			stainedglass.add(new ItemStack(Material.valueOf("RED_WOOL")));
+			stainedglass.add(new ItemStack(Material.valueOf("BLACK_WOOL")));
+		} catch (Exception a) {
+			for(int i = 0;i<=15;i++)
+			{
+				ItemStack wool = new ItemStack(Material.valueOf("WOOL"));
+				wool.setDurability((short) i);
+				stainedglass.add(wool);
 			}
 		}
 	}
--- src/pers/tany/customchunklimit/CommonlyWay.java
+++ src/pers/tany/customchunklimit/CommonlyWay.java
@@ -1,6 +1,7 @@
 package pers.tany.customchunklimit;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 import org.bukkit.Material;
@@ -128,11 +129,10 @@ public class CommonlyWay {
   		
 //		给予玩家物品，如果背包满则在原地生成掉落物
 		public static void GiveItem(Player player,ItemStack item) {
-			if(player.getInventory().firstEmpty()==-1) {
-				player.getWorld().dropItemNaturally(player.getLocation(), item);
-			} else {
-				player.getInventory().addItem(item);
-			}
+            HashMap<Integer,ItemStack> itemlist = new HashMap<Integer,ItemStack>(player.getInventory().addItem(item));
+            for(ItemStack i:itemlist.values()) {
+                player.getWorld().dropItemNaturally(player.getLocation(), i);
+            }
 		}
 		
 //		判断玩家是否为空手，空手则true
